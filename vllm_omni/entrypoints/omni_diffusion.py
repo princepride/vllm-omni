7c4c436caa051015a3b02cef5e0865863f9d08c9
princepride <wangzhipeng628@gmail.com>
2026-02-28 07:13:13 +0000

    fix a lot of bug
    Signed-off-by: princepride <wangzhipeng628@gmail.com>

--- vllm_omni/entrypoints/omni_diffusion.py
+++ vllm_omni/entrypoints/omni_diffusion.py
@@ -85,17 +85,19 @@ class OmniDiffusion:
                     od_config.model_class_name = "NextStep11Pipeline"
                 od_config.tf_model_config = TransformerConfig()
                 od_config.update_multimodal_support()
-            elif model_type == "glm-image" or "GlmImageForConditionalGeneration" in architectures:
-                pipeline_class = "GlmImagePipeline"
-            elif architectures and len(architectures) == 1:
-                pipeline_class = architectures[0]
+            else:
+                pipeline_class = None
+                if model_type == "glm-image" or "GlmImageForConditionalGeneration" in architectures:
+                    pipeline_class = "GlmImagePipeline"
+                elif architectures and len(architectures) == 1:
+                    pipeline_class = architectures[0]
 
-            if pipeline_class is None:
-                raise ValueError(f"Unknown model type: {model_type}, architectures: {architectures}")
+                if pipeline_class is None:
+                    raise ValueError(f"Unknown model type: {model_type}, architectures: {architectures}")
 
-            od_config.model_class_name = pipeline_class
-            od_config.tf_model_config = TransformerConfig()
-            od_config.update_multimodal_support()
+                od_config.model_class_name = pipeline_class
+                od_config.tf_model_config = TransformerConfig()
+                od_config.update_multimodal_support()
 
         self.engine: DiffusionEngine = DiffusionEngine.make_engine(od_config)
 
